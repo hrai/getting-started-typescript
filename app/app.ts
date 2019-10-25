556fcda5c81a85e3023ad8ec260c5b23e46f4d85
Hangjit Rai <rai.hangjit@gmail.com>
2019-10-25 13:47:42 +1100

    Removed Js Folder And Added A New Func

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,15 @@
 function startGame() {
   // starting a new game
+
+  const playerName: string = 'Avril';
+  logPlayer(playerName);
+
   var messagesElement = document.getElementById('messages');
   messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
+}
+
+function logPlayer(name:string) {
+  console.log(`New game starting for : ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
