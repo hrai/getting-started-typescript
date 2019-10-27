3c7e2e1799b2f6d1f190b1a3f73a72cd2abdfd04
Hangjit Rai <rai.hangjit@gmail.com>
2019-10-27 13:31:14 +1100

    Updated Webpack Compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  entry: './js/app.js',
-  // devtool: 'inline-source-map',
-  // resolve: {
-  //   extensions: [ '.tsx', '.ts', '.js' ]
-  // },
-  // module: {
-  //   rules: [
-  //     { test: /\.css$/, use: 'css-loader' },
-  //     { test: /\.ts$/, use: 'ts-loader' }
-  //   ]
-  // },
-  // output: {
-  //   filename: 'bundle.js'
-  // },
+  entry: './app/app.ts',
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: 'css-loader' },
+      { test: /\.ts$/, use: 'ts-loader' }
+    ]
+  },
+  output: {
+    filename: 'bundle.js'
+  },
   devServer: {
     inline: false
   }
